3a78933c58a62707af709f91c7a7f845e7cf1e13
Rahul <rajamohanty12345@gmail.com>
2022-03-28 22:56:13 +0530

    warnings removed

--- components/LayoutWrapper/Header.js
+++ components/LayoutWrapper/Header.js
@@ -8,7 +8,7 @@ const Header = () => {
     <>
       <div className="flex justify-between drop-shadow-md mt-3 mb-2 border-b-2 backdrop-blur-md">
         <div className="ml-14">
-          <Image width={186.27} height={48} src={logo} />
+          <Image alt="logo" width={186.27} height={48} src={logo} />
         </div>
         <div className="flex justify-between items-center text-base">
           <div className="pl-14">Skip the Hassle</div>
